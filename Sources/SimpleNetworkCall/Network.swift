d8b21fe8735a886ef029081aa4ea78b8bc25479e
dion dula <diondula2@gmail.com>
2021-06-14 20:10:44 +0200

    Removing Generic Y From Delete function

--- Sources/SimpleNetworkCall/Network.swift
+++ Sources/SimpleNetworkCall/Network.swift
@@ -23,7 +23,7 @@ public class Network: NSObject {
         fetchData(body: body, httpMethodType: HtppMethodType.POST, urlString: urlString, completion: completion)
     }
     //Nil Body in to Delete
-    public func delete<T : Decodable, Y : Encodable>(urlString: String,
+    public func delete<T : Decodable>(urlString: String,
                                                      completion: @escaping (Result<T, Error>) -> Void) {
         let nilInt: Int? = nil
         fetchData(body: nilInt, httpMethodType: HtppMethodType.DELETE, urlString: urlString, completion: completion)
